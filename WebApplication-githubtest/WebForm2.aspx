3276d98086abecef0a1e3b69ff6e72da53c2b938
mesameergaikwad <sameergaikwad1996@gmail.com>
2024-05-08 11:51:53 +0530

    create text box button

--- WebApplication-githubtest/WebForm2.aspx
+++ WebApplication-githubtest/WebForm2.aspx
@@ -13,6 +13,12 @@
              <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
             <asp:Button ID="Button1" runat="server" Text="Button" />
         </div>
+
+         <div>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+             <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            <asp:Button ID="Button2" runat="server" Text="Button" />
+        </div>
     </form>
 </body>
 </html>
--- WebApplication-githubtest/WebForm2.aspx.designer.cs
+++ WebApplication-githubtest/WebForm2.aspx.designer.cs
@@ -49,5 +49,32 @@ namespace WebApplication_githubtest
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
     }
 }
